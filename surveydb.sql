2dbbb31e26ec2efe265c083c9275132dd5c8cad9
Biri Komori <the.biri@gmail.com>
2015-06-07 14:36:40 -0400

    Updated the database export file to something more up to date. I also removed the dummy_data.sql file since it was both outdated and difficult to continue maintain. The database is pretty solid, and we can create users and databases on our own now so it's not really necessary to keep around anymore. (might even cause issues)

--- surveydb.sql
+++ surveydb.sql
@@ -1,23 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0-dev
+-- version 2.11.4
 -- http://www.phpmyadmin.net
 --
--- Host: 192.168.30.23
--- Generation Time: May 24, 2015 at 06:14 PM
--- Server version: 5.5.40-0+wheezy1
--- PHP Version: 5.6.7-1
+-- Host: localhost
+-- Generation Time: Jun 07, 2015 at 02:22 PM
+-- Server version: 5.1.57
+-- PHP Version: 5.2.17
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `surveydb`
+-- Database: `a9634422_survey`
 --
 
 -- --------------------------------------------------------
@@ -26,12 +19,20 @@ SET time_zone = "+00:00";
 -- Table structure for table `answers`
 --
 
-CREATE TABLE IF NOT EXISTS `answers` (
+CREATE TABLE `answers` (
   `question_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `survey_id` int(11) NOT NULL,
-  `answer` varchar(2500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `answer` varchar(2500) NOT NULL,
+  PRIMARY KEY (`user_id`,`survey_id`,`question_id`),
+  KEY `answers_fk1` (`question_id`),
+  KEY `answers_fk2` (`survey_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `answers`
+--
+
 
 -- --------------------------------------------------------
 
@@ -39,10 +40,18 @@ CREATE TABLE IF NOT EXISTS `answers` (
 -- Table structure for table `lkp_survey_user_rel`
 --
 
-CREATE TABLE IF NOT EXISTS `lkp_survey_user_rel` (
-  `relation_id` int(11) NOT NULL,
-  `relation_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `lkp_survey_user_rel` (
+  `relation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `relation_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`relation_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `lkp_survey_user_rel`
+--
+
+INSERT INTO `lkp_survey_user_rel` VALUES(1, 'maker');
+INSERT INTO `lkp_survey_user_rel` VALUES(2, 'taker');
 
 -- --------------------------------------------------------
 
@@ -50,12 +59,40 @@ CREATE TABLE IF NOT EXISTS `lkp_survey_user_rel` (
 -- Table structure for table `questions`
 --
 
-CREATE TABLE IF NOT EXISTS `questions` (
+CREATE TABLE `questions` (
   `question_id` int(11) NOT NULL,
   `survey_id` int(11) NOT NULL,
   `qtype_id` int(11) NOT NULL,
-  `question_text` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `question_text` varchar(100) NOT NULL,
+  PRIMARY KEY (`question_id`,`survey_id`),
+  KEY `questions_fk1` (`qtype_id`),
+  KEY `questions_fk2` (`survey_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_options`
+--
+
+CREATE TABLE `question_options` (
+  `option_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `survey_id` int(11) NOT NULL,
+  `option_text` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`survey_id`,`question_id`,`option_id`),
+  KEY `question_options_fk1` (`question_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `question_options`
+--
+
 
 -- --------------------------------------------------------
 
@@ -63,10 +100,19 @@ CREATE TABLE IF NOT EXISTS `questions` (
 -- Table structure for table `question_type`
 --
 
-CREATE TABLE IF NOT EXISTS `question_type` (
-  `qtype_id` int(11) NOT NULL,
-  `qtype_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `question_type` (
+  `qtype_id` int(11) NOT NULL AUTO_INCREMENT,
+  `qtype_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`qtype_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `question_type`
+--
+
+INSERT INTO `question_type` VALUES(1, 'True/False');
+INSERT INTO `question_type` VALUES(2, 'Multiple Choice (4)');
+INSERT INTO `question_type` VALUES(3, 'Fill In The Blank');
 
 -- --------------------------------------------------------
 
@@ -74,11 +120,17 @@ CREATE TABLE IF NOT EXISTS `question_type` (
 -- Table structure for table `surveys`
 --
 
-CREATE TABLE IF NOT EXISTS `surveys` (
-  `survey_id` int(11) NOT NULL,
+CREATE TABLE `surveys` (
+  `survey_id` int(11) NOT NULL AUTO_INCREMENT,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `survey_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `survey_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`survey_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `surveys`
+--
+
 
 -- --------------------------------------------------------
 
@@ -86,13 +138,23 @@ CREATE TABLE IF NOT EXISTS `surveys` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` VALUES(1, 'the.biri@gmail.com', 'FirstName', 'LastName', 'ceb6c970658f31504a901b89dcd3e461');
+INSERT INTO `users` VALUES(3, 'RDIBACCO@knights.ucf.edu', 'Richard', 'DiBacco', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `users` VALUES(4, 'alex@gmail.yo', 'alex', 'dibacco', '7694f4a66316e53c8cdd9d9954bd611d');
 
 -- --------------------------------------------------------
 
@@ -100,145 +162,17 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `user_surveys`
 --
 
-CREATE TABLE IF NOT EXISTS `user_surveys` (
+CREATE TABLE `user_surveys` (
   `user_id` int(11) NOT NULL,
   `survey_id` int(11) NOT NULL,
   `relation_id` int(11) NOT NULL,
-  `performed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
- CREATE TABLE `question_options` (
-`option_id` INT NOT NULL ,
-`survey_id` int(11) NOT NULL,
-`question_id` INT NOT NULL ,
-`option_text` VARCHAR( 100 ) NOT NULL ,
-PRIMARY KEY ( `option_id`,`question_id`,`survey_id` )
-) ENGINE=InnoDB; 
---
--- Indexes for dumped tables
---
+  `performed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`,`survey_id`,`relation_id`),
+  KEY `user_surveys_fk1` (`relation_id`),
+  KEY `user_surveys_fk2` (`survey_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Indexes for table `answers`
+-- Dumping data for table `user_surveys`
 --
 
-ALTER TABLE `answers` ADD PRIMARY KEY ( `user_id` , `survey_id` , `question_id` ) ;
---
--- Indexes for table `lkp_survey_user_rel`
---
-ALTER TABLE `lkp_survey_user_rel`
-  ADD PRIMARY KEY (`relation_id`);
-
---
--- Indexes for table `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`question_id`,`survey_id`),
-  ADD KEY `questions_fk1` (`qtype_id`),
-  ADD KEY `questions_fk2` (`survey_id`);
-
---
--- Indexes for table `question_type`
---
-ALTER TABLE `question_type`
-  ADD PRIMARY KEY (`qtype_id`);
-
---
--- Indexes for table `surveys`
---
-ALTER TABLE `surveys`
-  ADD PRIMARY KEY (`survey_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `user_surveys`
---
-ALTER TABLE `user_surveys`
-  ADD PRIMARY KEY (`user_id`,`survey_id`,`relation_id`),
-  ADD KEY `user_surveys_fk1` (`relation_id`),
-  ADD KEY `user_surveys_fk2` (`survey_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `lkp_survey_user_rel`
---
-ALTER TABLE `lkp_survey_user_rel`
-  MODIFY `relation_id` int(11) NOT NULL AUTO_INCREMENT;
---
---
--- AUTO_INCREMENT for table `question_type`
---
-ALTER TABLE `question_type`
-  MODIFY `qtype_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `surveys`
---
-ALTER TABLE `surveys`
-  MODIFY `survey_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `answers`
---
-ALTER TABLE `answers`
-  ADD CONSTRAINT `answers_fk1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `answers_fk2` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`survey_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `answers_fk3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
--- Constraints for table `questions`
---
-ALTER TABLE `questions`
-  ADD CONSTRAINT `questions_fk2` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`survey_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `questions_fk1` FOREIGN KEY (`qtype_id`) REFERENCES `question_type` (`qtype_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
--- Constraints for table `user_surveys`
---
-ALTER TABLE `user_surveys`
-  ADD CONSTRAINT `user_surveys_fk3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_surveys_fk1` FOREIGN KEY (`relation_id`) REFERENCES `lkp_survey_user_rel` (`relation_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_surveys_fk2` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`survey_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE `question_options`
-  ADD CONSTRAINT `question_options_fk1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
--- Preset Values
-INSERT INTO `lkp_survey_user_rel` (`relation_id` ,`relation_name`)
-VALUES (1 , 'maker');
-
-INSERT INTO `lkp_survey_user_rel`(`relation_id`,`relation_name`)
-VALUES (2 , 'taker');
-
-INSERT INTO `question_type` (`qtype_id` ,`qtype_name`)
-VALUES (1 , 'True/False');
-
-INSERT INTO `question_type` (`qtype_id` ,`qtype_name`)
-VALUES (2 , 'Multiple Choice (4)');
-
-INSERT INTO `question_type` (`qtype_id` ,`qtype_name`)
-VALUES (3 , 'Fill In The Blank');
-
-
- ALTER TABLE `question_options` CHANGE `option_id` `option_id` INT( 11 ) NOT NULL AUTO_INCREMENT 
